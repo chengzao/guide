cfb5129b4548e7833e2330e5478f7965e01ad4ec
chengzao <czhlink@163.com>
2019-06-05 12:16:43 +0800

    fix

--- docs/.vuepress/components/SpecialLayout.vue
+++ docs/.vuepress/components/SpecialLayout.vue
@@ -1,26 +1,42 @@
 <template>
   <div class="specialLayout">
-    <div class="specialLayout__center">时间：{{ updateTime }}</div>
+    <div class="specialLayout__center m10">当前时间：{{ time }}</div>
+    <Hitokoto/>
     <div class="specialLayout-content">
       <Content/>
     </div>
+    <div class="specialLayout__center">$page时间：{{ updateTime }}</div>
   </div>
 </template>
 
 <script>
 import { formatDateTime } from "../utils/tools";
-
+import Hitokoto from "./Hitokoto";
 export default {
-  mounted() {
-    // console.log(this.$page);
+  data() {
+    return {
+      time: formatDateTime(+new Date(), "YYYY-MM-DD hh:mm:ss")
+    };
+  },
+  components: {
+    Hitokoto
   },
   computed: {
     updateTime() {
       return (
-        this.$page.frontmatter.time ||
-        formatDateTime(+new Date(), "YYYY-MM-DD hh:mm:ss")
+        this.$page.frontmatter.time || formatDateTime(+new Date(), "YYYY-MM-DD")
       );
     }
+  },
+  methods: {
+    currentTime() {
+      this.time = formatDateTime(+new Date(), "YYYY-MM-DD hh:mm:ss");
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.currentTime();
+    }, 1000);
   }
 };
 </script>
@@ -28,6 +44,9 @@ export default {
 <style scoped>
 .specialLayout__center {
   text-align: center;
+}
+.specialLayout__center.m10 {
+  margin: 10px;
 }
 .specialLayout .specialLayout-content {
   text-align: center;
