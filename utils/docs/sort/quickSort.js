f408a5b4b6cff7b2f2f8d061c52226ba60036619
chengzao <czhlink@163.com>
2019-12-31 13:27:11 +0800

    quickSort

--- utils/docs/sort/quickSort.js
+++ utils/docs/sort/quickSort.js
@@ -48,7 +48,6 @@ const array1 = [5, 4, 3, 2, 1];
 console.log('quickSort1 ', quickSort1(array1));
 
 // 原地分区版快速排序实现
-// 快速排序
 const quickSort2 = (arr, left, right) => {
   let len = arr.length,
     partitionIndex;
@@ -56,25 +55,16 @@ const quickSort2 = (arr, left, right) => {
   right = typeof right != 'number' ? len - 1 : right;
 
   if (left < right) {
+    // 返回基准值的所在位置， 即索引
     partitionIndex = partition(arr, left, right);
+    // 可理解为：把数组拆分为 左边的数组值都比基准值小，右边的数组都比基准值大
+
+    // 比对基准值左边的数组
     quickSort2(arr, left, partitionIndex - 1);
+    // 比对基准值右边的数组
     quickSort2(arr, partitionIndex + 1, right);
   }
   return arr;
-};
-
-const partition = (arr, left, right) => {
-  //分区操作
-  let pivot = left, //设定基准值（pivot）
-    index = pivot + 1;
-  for (let i = index; i <= right; i++) {
-    if (arr[i] < arr[pivot]) {
-      swap(arr, i, index);
-      index++;
-    }
-  }
-  swap(arr, pivot, index - 1);
-  return index - 1;
 };
 
 const swap = (arr, i, j) => {
@@ -84,5 +74,43 @@ const swap = (arr, i, j) => {
   [arr[i], arr[j]] = [arr[j], arr[i]]
 };
 
-const array2 = [5, 4, 13, 2, 1];
+const partition = (arr, left, right) => {
+  //分区操作
+  let pivotIdx = left, //基准值（pivot）的索引
+    pivot = arr[pivotIdx], // 基准值（pivot）
+    index = pivotIdx + 1; // 初始index的位置
+
+  // index: 最终记录着数组中首次比基准值（pivot）大的最后索引位置
+
+  // i 取 index 的下一个数与之对比
+  for (let i = index; i <= right; i++) {
+    // 判断基准值与当前项的大小
+    if (arr[i] < pivot) {
+      // 如果比基准值小，将arr[i] 与 arr[index]交换
+      // 即 小于基准值的 与 大于基准值的 互相交换
+      swap(arr, i, index);
+      // index 下移一位
+      index++;
+    }
+  }
+  // 将基准值与最后一次小于它的值交换
+  swap(arr, pivotIdx, index - 1);
+
+  // 返回最后一次比基准值小的索引位置： index-1
+  return index - 1;
+};
+
+const array2 = [8, 5, 1, 13, 2, 6, 9, 21];
+
+// partitionIndex = partition(arr, left, right);
+// 执行 部分 结果
+// ...
+// 8, 5, 1, 13, 2, 6, 9, 21
+// 8, 5, 1, 2, 13, 6, 9, 21
+// 8, 5, 1, 2, 6, 13, 9, 21
+// 8, 5, 1, 2, 6, 13, 9, 21
+// ...
+// 6, 5, 1, 2, 8, 13, 9, 21 => swap(arr, pivotIdx, index - 1);
+
 console.log('quickSort2 ', quickSort2(array2));
+// => [ 1, 2, 5, 6, 8, 9, 13, 21 ]
