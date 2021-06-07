42fb7d55ed97a1fe54a01b8f570d0ad06a58b82c
chengzao <czhlink@163.com>
2021-06-07 18:12:51 +0800

    Update

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -2,7 +2,6 @@ module.exports = [
   [
     "vuepress-plugin-meilisearch",
     {
-      // hostUrl: "https://main-meilise-hxwkas5z3lnxw2bx-gtw.qovery.io",
       hostUrl: "https://booster.chengzao.workers.dev",
       apiKey: "1133481d50d6ebb46b5c167c4593e70706da4c77256f94ef3880b4c331cf1540",
       indexUid: "vuepress",
