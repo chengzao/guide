a64692a0aea18cfa7ca59d375bc765d0645bc958
chengzao <czhlink@163.com>
2021-06-18 23:51:23 +0800

    Update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,6 +6,7 @@ export default ({
   Vue,
   options, // 附加到根实例的一些选项
   router,
+  isServer,
   siteData // 站点元数据
 }) => {
   // Vue.use(Element)
@@ -18,4 +19,28 @@ export default ({
   //     next();
   //   }
   // });
+
+  router.beforeEach((to,from, next) => {
+    if(!isServer){
+      (function(w, doc,l){
+        if(w[l]) {
+          return w[l]
+        }else{
+          var body = doc.querySelector('body');
+          var sc = doc.createElement('noscript');
+          var ifr = doc.createElement('iframe');
+          ifr.src= "https://www.googletagmanager.com/ns.html?id=GTM-NRFTTPL";
+          ifr.width = 0;
+          ifr.height = 0;
+          ifr.style = "display:none;visibility:hidden"
+          sc.appendChild(ifr);
+          w[l] = sc;
+          body.insertBefore(sc,doc.querySelector('#app'));
+        }
+      })(window,document, 'GTMNRFTTPL');
+    }
+    next()
+  })
+
+
 };
