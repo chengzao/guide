13025c7167212dea73d4c8b1d47e271c453abd3b
chengzao <czhlink@163.com>
2022-02-08 18:45:57 +0800

    fix css

--- docs/.vuepress/components/todo/Todo.vue
+++ docs/.vuepress/components/todo/Todo.vue
@@ -276,7 +276,7 @@ button {
   font-weight: inherit;
   line-height: 1.4em;
   border: 0;
-  color: inherit;
+  color: #333;
   padding: 6px;
   border: 1px solid #999;
   box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
@@ -332,6 +332,7 @@ button {
   margin: 0;
   padding: 0;
   list-style: none;
+  color: #333;
 }
 
 .todo-list li {
